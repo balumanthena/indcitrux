2e280e1fd88bc69ac959ec40279ec4b970f6c614
balumanthena <balumanthena18@gmail.com>
2025-07-09 20:55:15 +0530

    your commit message

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -3,7 +3,8 @@
 import Slider from "react-slick";
 import Image from "next/image";
 import { useMediaQuery } from "react-responsive";
-
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -31,71 +32,84 @@ const SliderOne = () => {
   return (
     <div>
       <Slider {...settings}>
-        <div className="rounded-md px-2 md:p-10">
-          <Image
-            priority
-            src="/images/business.jpeg"
-            alt="logo"
-            width={500}
-            height={500}
-            className="rounded-2xl"
-          />
-        </div>
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/business.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="rounded-2xl"
+            />
+          </div>
+        </>
 
-        <div className="rounded-md px-2 md:p-10">
-          <Image
-            priority
-            src="/images/coffe.jpeg"
-            alt="logo"
-            width={500}
-            height={500}
-            className="rounded-2xl"
-          />
-        </div>
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/coffe.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="rounded-2xl"
+            />
+          </div>
+        </>
 
-        <div className="rounded-md px-2 md:p-10">
-          <Image
-            priority
-            src="/images/small-business-3.webp"
-            alt="logo"
-            width={500}
-            height={500}
-            className="rounded-2xl"
-          />
-        </div>
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/small-business-3.webp"
+              alt="logo"
+              width={500}
+              height={500}
+              className="rounded-2xl"
+            />
+          </div>
+        </>
 
-        <div className="rounded-md px-2 md:p-10">
-          <Image
-            priority
-            src="/images/man.jpeg"
-            alt="logo"
-            width={500}
-            height={500}
-            className="rounded-2xl"
-          />
-        </div>
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/man.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="rounded-2xl"
+            />
+          </div>
+        </>
 
-        <div className="rounded-md px-2 md:p-10">
-          <Image
-            priority
-            src="/images/image-business.jpeg"
-            alt="logo"
-            width={500}
-            height={500}
-            className="rounded-2xl"
-          />
-        </div>
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/image-business.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="rounded-2xl"
+            />
+          </div>
+        </>
 
-        <div className="rounded-md px-2 md:p-10">
-          <Image
-            priority
-            src="/images/new-image.jpeg"
-            alt="new logo"
-            width={500}
-            height={500}
-            className="rounded-2xl"
-          />
-        </div>
+        {/* Added extra image */}
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/new-image.jpeg"  // <-- Your new image path here
+              alt="new logo"
+              width={500}
+              height={500}
+              className="rounded-2xl"
+            />
+          </div>
+        </>
       </Slider>
     </div>
   );
